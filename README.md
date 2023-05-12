1348e74efbe976059176165528924a18ef9fd8c9
johnnywilly16 <128432294+johnnywilly16@users.noreply.github.com>
2023-05-12 16:09:28 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# entrevistajs2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-p298kn)
---
+++ script.js
@@ -0,0 +1,23 @@
+// *** DESENVOLVA SEU CÓDIGO ABAIXO ***
+let NumberTest = [65,13,96,98,78,45];
+//console.log(NumberTest[0])
+console.log(NumberTest[NumberTest.length -1])
+console.log(NumberTest.length -1)
+for (let i =0; i < NumberTest.length; i++){
+ console.log(NumberTest[i])
+}
+
+let casa = {
+  tipo: "Casa térrea",
+  numero: 125,
+  nomes: [
+    "Fernanda", "José"
+  ]
+}
+//console.log(casa.Numero)
+//console.log(casa.Nomes[0])
+
+let teste = document.getElementsByClassName("tech-list")[0];
+// console.log(teste);
+
+document.createElement("li")
